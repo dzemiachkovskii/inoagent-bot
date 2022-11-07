7d0c0a4074e69fd2607b2a5602a1975958bc12c0
305 <305@op9.local>
2022-11-07 12:34:51 +0300

    add requirements & procfile

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 import os
 
 import telebot
-from flask import Flask
+from flask import Flask, request
 from telebot import TeleBot, types
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
@@ -16,3 +16,18 @@ bot.message_handler(content_types=['text'])
 
 def main(m):
     bot.send_message(m.chat.id, m)
+
+
+@server.route('//' + BOT_TOKEN, methods=['POST'])
+def redirect_message():
+    json_string = request.get_data().decode("utf-8")
+    update = types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+
+APP_URL = os.environ.get('APP_URL')
+if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
